e0670338df3e393a00ade3403f8946011a912080
Aquarius4649 <134042368+Aquarius4649@users.noreply.github.com>
2023-07-10 06:03:48 +0000

    image commit_third

--- next_app/src/components/footer.tsx
+++ next_app/src/components/footer.tsx
@@ -8,35 +8,29 @@ const Footer: React.FC = () => {
         <div className="w-full">
           <div>
             <div className="flex text-center w-full">
-              <div className="text-slate-300 flex-1">
-                <a href="footer_home.png">
+              <div className="flex justify-center text-slate-300 flex-1">
                 <Image className=''
                 src="/footer_home.png"
                 width={50}
                 height={50}
                 alt="home"
                 />
-                </a>
               </div>
               <div className="text-slate-300 flex-1">
-                <a href="footer_chat.png">
                 <Image className=''
                 src="/footer_chat.png"
                 width={50}
                 height={50}
                 alt="chat"
                 />
-                </a>
               </div>
               <div className="text-slate-300 flex-1">
-              <a href="footer_context.png">
                 <Image className=''
                 src="/footer_context.png"
                 width={50}
                 height={50}
                 alt="context"
                 />
-                </a>
               </div>
             </div>
             <div className="flex text-center w-full">
