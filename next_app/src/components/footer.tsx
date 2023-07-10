f97d107b27b51f7e88c600b6414da9e0a640a26e
Aquarius4649 <134042368+Aquarius4649@users.noreply.github.com>
2023-07-10 04:50:50 +0000

    image_commit

--- next_app/src/components/footer.tsx
+++ next_app/src/components/footer.tsx
@@ -1,16 +1,48 @@
 import React from 'react';
+import Image from 'next/image';
 
 const Footer: React.FC = () => {
   return (
     <>
-      <footer className='absolute bottom-0 bg-slate-500 w-full'>
+      <footer className='absolute bottom-0 bg-slate-500 w-full h-1/10'>
         <div className="w-full">
           <div>
-            
-            <div className="flex w-full">
-              <div className="pr-10 pl-10 text-slate-300">HOME</div>
-              <div className="pr-10 text-slate-300">CHAT</div>
-              <div className="pr-10 text-slate-300">CONTACT</div>
+            <div className="flex text-center w-full">
+              <div className="pr-10 pl-10 text-slate-300 flex-1">
+                <a href="footer_home.png">
+                <Image className=''
+                src="/footer_home.png"
+                width={50}
+                height={50}
+                alt="home"
+                />
+                </a>
+              </div>
+              <div className="pr-10 text-slate-300 flex-1">
+                <a href="footer_chat.png">
+                <Image className=''
+                src="/footer_chat.png"
+                width={50}
+                height={50}
+                alt="chat"
+                />
+                </a>
+              </div>
+              <div className="pr-10 text-slate-300 flex-1">
+              <a href="footer_context.png">
+                <Image className=''
+                src="/footer_context.png"
+                width={50}
+                height={50}
+                alt="context"
+                />
+                </a>
+              </div>
+            </div>
+            <div className="flex text-center w-full">
+              <div className="pr-10 pl-10 text-slate-300 flex-1">HOME</div>
+              <div className="pr-10 text-slate-300 flex-1">CHAT</div>
+              <div className="pr-10 text-slate-300 flex-1">CONTACT</div>
             </div>
           </div>
         </div>
--- next_app/src/components/hello.tsx
+++ next_app/src/components/hello.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import Image from 'next/image';
 
 const Test: React.FC = () => {
   return (
     <>
       <p>Component</p>
-
+      <div>
+      <a href="">
+      <Image
+      src="/profile.png"
+      width={500}
+      height={500}
+      alt="a"
+      />
+      </a>
+      </div>
     </>
   );
 };
