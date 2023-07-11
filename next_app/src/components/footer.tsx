c39860aeac25148e4c804a6c78863026885933fc
Aquarius4649 <134042368+Aquarius4649@users.noreply.github.com>
2023-07-11 07:00:48 +0000

    footer_commit_fifth

--- next_app/src/components/footer.tsx
+++ next_app/src/components/footer.tsx
@@ -7,7 +7,7 @@ const Footer: React.FC = () => {
       <footer className='absolute bottom-0 bg-slate-500 w-full h-1/10'>
         <div className="w-full">
           <div>
-            <div className="flex text-center w-full">
+            <div className="flex pt-2 text-center w-full">
               <div className="pr-10 pl-10 flex justify-center text-slate-300 flex-1">
                 <Image className=''
                 src="/footer_home.png"
@@ -16,7 +16,7 @@ const Footer: React.FC = () => {
                 alt="home"
                 />
               </div>
-              <div className="justify-center text-slate-300 flex-1">
+              <div className="pr-10 flex justify-center text-slate-300 flex-1">
                 <Image className=''
                 src="/footer_chat.png"
                 width={50}
@@ -24,7 +24,7 @@ const Footer: React.FC = () => {
                 alt="chat"
                 />
               </div>
-              <div className="justify-center text-slate-300 flex-1">
+              <div className="pr-10 flex justify-center text-slate-300 flex-1">
                 <Image className=''
                 src="/footer_context.png"
                 width={50}
@@ -34,9 +34,9 @@ const Footer: React.FC = () => {
               </div>
             </div>
             <div className="flex text-center w-full">
-              <div className="pr-10 pl-10 text-slate-300 flex-1">HOME</div>
-              <div className="pr-10 text-slate-300 flex-1">CHAT</div>
-              <div className="pr-10 text-slate-300 flex-1">CONTACT</div>
+              <div className="pr-10 pl-10 pt-1 pb-1 text-slate-300 text-xs flex-1">HOME</div>
+              <div className="pr-10 pt-1 pb-1 text-slate-300 text-xs flex-1">CHAT</div>
+              <div className="pr-10 pt-1 pb-1 text-slate-300 text-xs flex-1">CONTACT</div>
             </div>
           </div>
         </div>
