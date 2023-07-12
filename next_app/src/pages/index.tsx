13788be77fc54e10870abea8599ee57888b7005c
stundet-ops <gogakuec@mail.com>
2023-07-13 02:20:09 +0900

    updated

--- next_app/src/pages/index.tsx
+++ next_app/src/pages/index.tsx
@@ -5,6 +5,7 @@ import Remark from '@/components/remark';
 import {PromptSuggestions} from '@/lib/suggestions'
 import React from 'react';
 import Suggestion from '@/components/chat_suggestion';
+import { verify } from 'crypto';
 
 export default function Home() {
   const backend_url = "http://localhost:4000"
@@ -96,74 +97,84 @@ export default function Home() {
   } 
 
   const GetCorrectness = async (id: string,queyr:string) => {
-    try {
-        const response = await fetch(backend_url+'/llama_evaluate', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-            //must change id
-                "id": "8da4d957-d9e4-48f1-a2ff-ad717314f43a",
-                "query":"どのような人材を目指していますか 日本語で回答して"
-            }),
-        });
+      try {
+          const response = await fetch(backend_url+'/llama_evaluate', {
+              method: 'POST',
+              headers: {
+                  'Content-Type': 'application/json'
+              },
+              body: JSON.stringify({
+              //must change id
+                  "id": "8da4d957-d9e4-48f1-a2ff-ad717314f43a",
+                  "query":"どのような人材を目指していますか 日本語で回答して"
+              }),
+          });
 
-        // レスポンスをチェックします。
-        if (!response.ok) {
-            throw new Error(`HTTP error! status: ${response.status}`);
-        }
+          // レスポンスをチェックします。
+          if (!response.ok) {
+              throw new Error(`HTTP error! status: ${response.status}`);
+          }
 
-        // データをパースします。
-        const data = await response.json();
+          // データをパースします。
+          const data = await response.json();
 
-        // データをリターンします。
-        return data.eval_result;
-    } catch (error) {
-      const err = error as Error;
-      console.error('Error:', err);
-      return { error: err.message };
-  }
+          // データをリターンします。
+          return data.eval_result;
+      } catch (error) {
+        const err = error as Error;
+        console.error('Error:', err);
+        return { error: err.message };
+    }
+  };
 
-};
+  const verification_class = "my-4 rounded-lg shadow-md "
 
   return (
     <div>
       <h1>Sample</h1>
       <h1>Streaming Data Display</h1>
-    {chatLog.map((log, index) => 
-      <>
-        <Remark key={index} remark={{message: log.message, user: 1}}/>
-        {log.response === undefined 
-          ? <StreamResponse key={`resp-${index}`} chat={log} setLoading={setLoading} setChat={setChat} />
-          : <Remark key={`resp-${index}`} remark={{user: 0, message: log.response}}/>
-        }
-        {
-          log.verification === undefined 
-          ? <p onClick={() => handleVerificationClick(log.id,log.message)}>検証する</p>
-          : (
-            <>
-              {log.verification?.correctness && 
-                <p>
-                  Correctness Ratio: 
-                  {`${log.verification.correctness.filter(c => c === 'YES').length} / ${log.verification.correctness.length}`}
-                </p>
+    <div className='w-full'>
+      {chatLog.map((log, index) => 
+        <div className={log.verification === undefined ?'flex w-full' :'flex flex-row-reverse my-4'}>
+          <div className={log.verification === undefined ?'flex justify-center w-full' :'w-1/3 flex items-center' }>
+            {log.verification === undefined ?<p onClick={() => handleVerificationClick(log.id,log.message)} className='my-auto'>検証する</p> :<></>}
+            <div className={log.verification ===undefined ?'flex flex-col w-1/2 items-center':'w-full'}>
+              <Remark key={index} remark={{message: log.message, user: 1}}/>
+              {log.response === undefined 
+                ? <StreamResponse key={`resp-${index}`} chat={log} setLoading={setLoading} setChat={setChat} />
+                : <Remark key={`resp-${index}`} remark={{user: 0, message: log.response}}/>
               }
-              {log.verification.nodes?.map((node, i) => (
-                <React.Fragment key={i}>
-                  <div className={log.verification!.correctness![i] === 'YES'? "bg-green-300":"bg-red-300"}>
-                    <p>
-                      Correctness: {log.verification!.correctness ? log.verification!.correctness[i] : "Not available"}
-                    </p>
-                    <p>{node}</p>
-                  </div>
-                </React.Fragment>
-              ))}
-            </>
-          )
-        }
-      </>
-    )}
+            </div>
+          </div>
+          {
+            log.verification === undefined 
+            ? <></>
+            : (
+              <div className='w-2/3'>
+                {log.verification?.correctness && 
+                  <p>
+                    Correctness Ratio: 
+                    {`${log.verification.correctness.filter(c => c === 'YES').length} / ${log.verification.correctness.length}`}
+                  </p>
+                }
+                {log.verification.nodes?.map((node, i) => (
+                  <React.Fragment key={i}>
+                    <div className={`${verification_class} ${log.verification!.correctness![i] === 'YES'? "bg-green-300":"bg-red-300"}`}>
+                      <div className='p-3'>
+                        <p>
+                          Correctness: {log.verification!.correctness ? log.verification!.correctness[i] : "Not available"}
+                        </p>
+                        <p className='py-3 whitespace-normal break-words'>{node}</p>
+                      </div>
+                    </div>
+                  </React.Fragment>
+                ))}
+              </div>
+            )
+          }
+        </div>
+      )}
+    </div>
     <Suggestion setMessage = {setMessage}/>
     <form onSubmit={(e) => {
       e.preventDefault();
